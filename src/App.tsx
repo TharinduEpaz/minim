7539d080e2275a4d550b00213da3d973cfdb1809
TharinduEpaz <epazingha@gmail.com>
2026-03-13 13:35:30 +0530

    feat : impl new sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ export function App() {
   }, []);
 
   const [settingsOpen, setSettingsOpen] = useState<boolean>(false);
-  const [bookmarksSidebarOpen, setBookmarksSidebarOpen] = useState<boolean>(true);
+  const [bookmarksSidebarOpen, setBookmarksSidebarOpen] = useState<boolean>(false);
   const [droppingWidgetData, setDroppingWidgetData] = useState<string | null>(
     null
   );
